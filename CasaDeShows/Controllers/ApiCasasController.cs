084f5ddd243ec4d27b1dba39db9740d069b4dece
gustavo <gu_kiub@hotmail.com>
2020-03-03 15:03:41 -0300

    Initial commit

---
+++ CasaDeShows/Controllers/ApiCasasController.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Linq;
+using CasaDeShows.Data;
+using Microsoft.AspNetCore.Mvc;
+using CasaDeShows.Models;
+
+namespace CasaDeShows.Models
+{
+    [Route("api/v1/casas")]
+    [ApiController]
+    public class ApiCasasController : ControllerBase
+    {
+        readonly ApplicationDbContext _context;
+
+
+        public ApiCasasController(ApplicationDbContext context){
+            _context = context;
+        }
+        
+        /// <summary>
+        /// Recupera todas as casas
+        /// </summary>
+        [HttpGet]
+        public IActionResult Get(){
+            var casas = _context.casasDeShow.ToList();
+            return Ok(new{casas});
+        }
+
+
+        /// <summary>
+        /// Recupera uma casa de show
+        /// </summary>
+        [HttpGet("{id}")]
+        public IActionResult Get(int id){
+            try
+            {
+                var casa = _context.casasDeShow.First(p => p.Id == id);
+                return Ok(casa);
+            }
+            catch (System.Exception)
+            {
+                Response.StatusCode = 404;
+                return new ObjectResult("");
+            }
+        }
+
+
+        /// <summary>
+        /// Cria uma nova casa de show.
+        /// </summary>
+        [HttpPost]
+        public IActionResult Post([FromBody] casaTemp CasaTemp){
+            if(CasaTemp.Nome == null){
+                Response.StatusCode = 400;
+                return new ObjectResult(new {msg = "O nome da casa precisa conter mais de 1 caracter."});
+            }
+
+            if(CasaTemp.Local == null){
+                Response.StatusCode = 400;
+                return new ObjectResult(new {msg = "O nome do local precisa conter mais de 1 caracter."});
+            }
+            
+            CasasDeShow c = new CasasDeShow();
+            c.Nome = CasaTemp.Nome;
+            c.Local = CasaTemp.Local;
+            _context.casasDeShow.Add(c);
+            _context.SaveChanges();
+
+            Response.StatusCode = 201;
+            return new ObjectResult("");
+        }
+
+
+
+        /// <summary>
+        /// Deleta uma casa de show
+        /// </summary>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id){
+            try
+            {
+                var casa = _context.casasDeShow.First(p => p.Id == id);
+                _context.casasDeShow.Remove(casa);
+                _context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                Response.StatusCode = 404;
+                return new ObjectResult("");
+            }
+        }
+
+
+        /// <summary>
+        /// Altera uma casa de show
+        /// </summary>
+        [HttpPatch]
+        public IActionResult Patch([FromBody] casaParaAtualizar casa){
+            if (casa.Id > 0){
+                try
+                {
+                    var p = _context.casasDeShow.First(ptemp => ptemp.Id == casa.Id);
+
+                    if(p != null){
+                        
+                        //editar 
+                        //operador ternario: condição ? faz algo : faz outra coisa
+                        p.Nome = casa.Nome != null ? casa.Nome : p.Nome;
+                        p.Local = casa.Local != null ? casa.Local : p.Local;
+
+                        _context.SaveChanges();
+                        return Ok();
+
+                    } else {
+                        Response.StatusCode = 400;
+                        return new ObjectResult(new {msg = "casa não encontrada"});
+                    }
+                }
+                catch (Exception)
+                {
+                    Response.StatusCode = 400;
+                    return new ObjectResult(new {msg = "casa não encontrada"});
+                }
+                
+            }else {
+                Response.StatusCode = 400;
+                return new ObjectResult(new {msg = "O Id da casa é invalido"});
+            }
+        }
+
+
+        public class casaTemp {
+            public string Nome {get; set;}
+            public string Local {get; set;}
+        }
+
+        public class casaParaAtualizar {
+            public int Id {get; set;}
+            public string Nome {get; set;}
+            public string Local {get; set;}
+        }
+    }
+}
---
+++ CasaDeShows/Startup.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
+using CasaDeShows.Data;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using CasaDeShows.Config;
+using System.IO;
+using System.Reflection;
+
+namespace CasaDeShows
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseMySql(
+                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDefaultIdentity<IdentityUser>(config => {
+                config.Password.RequireNonAlphanumeric = false;
+                config.Password.RequireUppercase = false;
+                config.Password.RequireLowercase = false;
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+
+            // injeção de dependencia passa o dataservice para toda a aplicação, necessário para gerar o banco automaticamente
+            services.AddTransient<IDataService, DataService>();
+            services.AddAuthorization(options => options.AddPolicy("Administrador", policy => policy.RequireClaim("Admin", "True")));
+        
+            services.AddSwaggerGen(config => {
+                config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo {Title = "API Casa de Shows", Version = "v1"});
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                config.IncludeXmlComments(xmlPath);
+            });
+        
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IServiceProvider serviceProvider)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseSwagger(config => {
+                config.RouteTemplate = "doc/{documentName}/swagger.json";
+            }); // gera um arquivo json // swagger.json
+            app.UseSwaggerUI(config => {  // Views HTML do swagger
+                config.SwaggerEndpoint("/doc/v1/swagger.json", "v1 docs");
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+            // codigo para gerar o banco automaticamente
+            serviceProvider.GetService<IDataService>().InicializaDB();
+        }
+    }
+}
